5dd1fbf0c0dabcffd4f1b40c8faa79cf33e064d3
Pedro-Tejero-Diez <pedrete3000@hotmail.com>
2023-10-22 12:37:55 +0200

    update3 home 23 oct 2023

--- wait-for-mysql.bat
+++
@@ -1,9 +0,0 @@
-:loop
-echo Waiting for MySQL to start...
-timeout /t 5 > NUL
-mysqladmin ping -hmysql -uroot -prootPWD > NUL 2>&1
-if %errorlevel% == 0 goto exit_loop
-goto loop
-
-:exit_loop
-echo MySQL is up and running.
