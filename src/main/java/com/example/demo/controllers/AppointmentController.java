00ca0548dd727a7a550fb1948937a6377093f95a
Pedro-Tejero-Diez <pedrete3000@hotmail.com>
2023-11-06 06:53:53 +0100

    update home 6 nov 2023

--- src/main/java/com/example/demo/controllers/AppointmentController.java
+++ src/main/java/com/example/demo/controllers/AppointmentController.java
@@ -6,6 +6,7 @@ import com.example.demo.entities.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,62 +22,77 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class AppointmentController {
 
-	@Autowired
-	AppointmentRepository appointmentRepository;
-	//@Autowired
-	//RoomRepository roomRepository;
+    @Autowired
+    AppointmentRepository appointmentRepository;
+    //@Autowired
+    //RoomRepository roomRepository;
 
-	@GetMapping("/appointments")
-	public ResponseEntity<List<Appointment>> getAllAppointments() {
-		List<Appointment> appointments = new ArrayList<>();
+    @GetMapping("/appointments")
+    public ResponseEntity<List<Appointment>> getAllAppointments() {
+        List<Appointment> appointments = new ArrayList<>();
 
-		appointmentRepository.findAll().forEach(appointments::add);
+        appointmentRepository.findAll().forEach(appointments::add);
 
-		if (appointments.isEmpty()) {
-			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-		}
+        if (appointments.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
 
-		return new ResponseEntity<>(appointments, HttpStatus.OK);
-	}
+        return new ResponseEntity<>(appointments, HttpStatus.OK);
+    }
 
-	@GetMapping("/appointments/{id}")
-	public ResponseEntity<Appointment> getAppointmentById(@PathVariable("id") long id) {
-		Optional<Appointment> appointment = appointmentRepository.findById(id);
+    @GetMapping("/appointments/{id}")
+    public ResponseEntity<Appointment> getAppointmentById(@PathVariable("id") long id) {
+        Optional<Appointment> appointment = appointmentRepository.findById(id);
 
-		if (appointment.isPresent()) {
-			return new ResponseEntity<>(appointment.get(), HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-	}
+        if (appointment.isPresent()) {
+            return new ResponseEntity<>(appointment.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 
-	@PostMapping("/appointment")
-	public ResponseEntity<List<Appointment>> createAppointment(@RequestBody Appointment appointment) {
-		// author: Pedro Tejero
-		appointmentRepository.save(appointment);
-		List<Appointment> appointments = appointmentRepository.findAll();
-		return new ResponseEntity<>(appointments, HttpStatus.OK);
-	}
+    @PostMapping("/appointment")
+    public ResponseEntity<List<Appointment>> createAppointment(@RequestBody Appointment appointment) {
+        // author: Pedro Tejero
+        List<Appointment> appointments = appointmentRepository.findAll();
+		//check errors in date
+        if (appointment.getStartsAt().equals(appointment.getFinishesAt())) {
+            return new ResponseEntity<>(appointments, HttpStatus.BAD_REQUEST);
+        }
+		//check there are no conflicts
+        int i = 0;
+        boolean conflict;
+        conflict = false;
+        while (!conflict && i < appointments.size()) {
+            conflict = appointment.overlaps(appointments.get(i));
+            i = i + 1;
+        }
+        if (!conflict) {
+            appointmentRepository.save(appointment);
+            return new ResponseEntity<>(appointments, HttpStatus.OK);
+        } else return new ResponseEntity<>(appointments, HttpStatus.NOT_ACCEPTABLE);
+    }
 
-	@DeleteMapping("/appointments/{id}")
-	public ResponseEntity<HttpStatus> deleteAppointment(@PathVariable("id") long id) {
 
-		Optional<Appointment> appointment = appointmentRepository.findById(id);
+    @DeleteMapping("/appointments/{id}")
+    public ResponseEntity<HttpStatus> deleteAppointment(@PathVariable("id") long id) {
 
-		if (!appointment.isPresent()) {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
+        Optional<Appointment> appointment = appointmentRepository.findById(id);
 
-		appointmentRepository.deleteById(id);
+        if (!appointment.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
 
-		return new ResponseEntity<>(HttpStatus.OK);
+        appointmentRepository.deleteById(id);
 
-	}
+        return new ResponseEntity<>(HttpStatus.OK);
 
-	@DeleteMapping("/appointments")
-	public ResponseEntity<HttpStatus> deleteAllAppointments() {
-		appointmentRepository.deleteAll();
-		return new ResponseEntity<>(HttpStatus.OK);
-	}
+    }
+
+    @DeleteMapping("/appointments")
+    public ResponseEntity<HttpStatus> deleteAllAppointments() {
+        appointmentRepository.deleteAll();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
