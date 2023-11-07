c6bd4a881ef1dfcad9f9ef3024bae7de87d701ec
Pedro-Tejero-Diez <pedrete3000@hotmail.com>
2023-11-07 06:58:30 +0100

    update home 7 nov 2023

--- wait-for-mysql.sh
+++ wait-for-mysql.sh
@@ -3,8 +3,9 @@
 # Wait for the MySQL database to be up and running
 until mysqladmin ping -h"$1" --silent; do
     echo 'Waiting for MySQL to be ready...'
-    sleep 2
+    sleep 3
 done
 
 # Run the Java application
-exec "$2" "${@:3}"
+#exec "$2" "/app/app.war"
+exec java -jar /app/app.war
